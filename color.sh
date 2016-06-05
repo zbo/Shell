93538d84a6d11f562b19d093e024d536b39a3fd7
superzbo <zbo2@qq.com>
2016-06-05 18:19:04 +0800

    add color echo

---
+++ color.sh
@@ -0,0 +1,16 @@
+function echo_green(){
+  echo -e "\e[1;32m$1\e[0m"
+}
+function echo_red(){
+  echo -e "\e[1;31m$1\e[0m"
+}
+function echo_yellow(){
+  echo -e "\e[1;33m$1\e[0m"
+}
+function echo_blue(){
+  echo -e "\e[1;34m$1\e[0m"
+}
+function echo_cyan(){
+  echo -e "\e[1;36m$1\e[0m"
+}
+
